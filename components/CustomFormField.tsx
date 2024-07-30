e6f68468202ef9400cdb670ac09a8d0bff821d92
marquesmaycon <mayconmarquesh@gmail.com>
2024-07-29 21:16:15 -0300

    Input field type date-picker

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -1,16 +1,11 @@
 import React from "react"
-import 'react-phone-number-input/style.css'
-import PhoneInput from 'react-phone-number-input'
-import { E164Number } from 'libphonenumber-js'
+import "react-phone-number-input/style.css"
+import PhoneInput from "react-phone-number-input"
+import { E164Number } from "libphonenumber-js"
+import "react-datepicker/dist/react-datepicker.css"
+import DatePicker from "react-datepicker"
 
-import {
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "./ui/input"
 import { Control } from "react-hook-form"
 import { FormFieldType } from "./forms/PatientForm"
@@ -32,45 +27,49 @@ type CustomProps = {
 }
 
 const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
-  const { fieldType, iconSrc, iconAlt, placeholder, disabled } = props
+  const { fieldType, iconSrc, iconAlt, placeholder, disabled, showTimeSelect, dateFormat } = props
 
   switch (fieldType) {
     case FormFieldType.INPUT:
       return (
         <div className="flex rounded-md border border-dark-500 bg-dark-400">
-          {iconSrc && (
-            <Image
-              src={iconSrc}
-              height={24}
-              width={24}
-              alt={iconAlt || "icon"}
-              className="ml-2"
-            />
-          )}
+          {iconSrc && <Image src={iconSrc} height={24} width={24} alt={iconAlt || "icon"} className="ml-2" />}
           <FormControl>
-            <Input
-              {...field}
-              placeholder={placeholder}
-              disabled={disabled}
-              className="shad-input border-0"
+            <Input {...field} placeholder={placeholder} disabled={disabled} className="shad-input border-0" />
+          </FormControl>
+        </div>
+      )
+    case FormFieldType.PHONE_INPUT:
+      return (
+        <FormControl>
+          <PhoneInput
+            defaultCountry="BR"
+            placeholder={placeholder}
+            international
+            withCountryCallingCode
+            value={field.value as E164Number | undefined}
+            onChange={field.onChange}
+            className="input-phone"
+          />
+        </FormControl>
+      )
+    case FormFieldType.DATE_PICKER:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          <Image src="/assets/icons/calendar.svg" height={24} width={24} alt="calendar" className="ml-2" />
+          <FormControl>
+            <DatePicker
+              selected={field.value}
+              onChange={(date) => field.onChange(date)}
+              showTimeSelect={showTimeSelect}
+              dateFormat={dateFormat ?? "dd/MM/yyyy"}
+              timeInputLabel="Time:"
+              wrapperClassName="date-picker"
+
             />
           </FormControl>
         </div>
-			)
-		case FormFieldType.PHONE_INPUT:
-			return (
-				<FormControl>
-					<PhoneInput
-						defaultCountry="BR"
-						placeholder={placeholder}
-						international
-						withCountryCallingCode
-						value={field.value as E164Number | undefined}
-						onChange={field.onChange}
-						className="input-phone"
-					/>
-				</FormControl>
-			)
+      )
     default:
       break
   }
@@ -84,14 +83,9 @@ const CustomFormField = (props: CustomProps) => {
       name={name}
       render={({ field }) => (
         <FormItem className="flex-1">
-          {fieldType !== FormFieldType.CHECKBOX && label && (
-            <FormLabel>{label}</FormLabel>
-          )}
+          {fieldType !== FormFieldType.CHECKBOX && label && <FormLabel>{label}</FormLabel>}
 
-          <RenderField
-            field={field}
-            props={props}
-          />
+          <RenderField field={field} props={props} />
 
           <FormMessage className="shad-error" />
         </FormItem>
--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -5,13 +5,16 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
-import { Form } from "@/components/ui/form"
+import { Form, FormControl } from "@/components/ui/form"
 import { userFormValidation } from "@/lib/validation"
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.actions"
 import { FormFieldType } from "./PatientForm"
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import { GenderOptions } from "@/constants"
+import { Label } from "../ui/label"
 
 const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter()
@@ -49,9 +52,7 @@ const RegisterForm = ({ user }: { user: User }) => {
 
         <section className="space-y-6">
           <div className="mb-9 space-y-1">
-            <h2 className="sub-header">
-              Personal Information
-            </h2>
+            <h2 className="sub-header">Personal Information</h2>
           </div>
         </section>
 
@@ -64,6 +65,65 @@ const RegisterForm = ({ user }: { user: User }) => {
           iconSrc="/assets/icons/user.svg"
           iconAlt="user"
         />
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.INPUT}
+            name="email"
+            label="Email"
+            placeholder="johndoe@jd.com"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="email"
+          />
+
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.PHONE_INPUT}
+            name="phone"
+            label="Phone number"
+            placeholder="00 0 0000-0000"
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.DATE_PICKER}
+            name="birthDate"
+            label="Date of Birth"
+            placeholder="johndoe@jd.com"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="email"
+          />
+
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.SKELETON}
+            name="gender"
+            label="Gender"
+            renderSkeleton={(field) => (
+              <FormControl>
+                <RadioGroup className="flex h-11 gap-6 xl:justify-between" onValueChange={field.onChange} defaultValue={field.value}>
+                  {GenderOptions.map((option) => (
+                    <div key={option} className="radio-group">
+                      <RadioGroupItem value={option} id={option}>
+                        <Label htmlFor={option} className="cursor-pointer">
+                          {option}
+                        </Label>
+                      </RadioGroupItem>
+                    </div>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            )}
+          />
+        </div>
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
